55e2c75e060f6850520917106c4f696a0852abbc
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-19 13:08:05 +0800

    更新 index.js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ export default {
 	async fetch(req, env, ctx) {
 		try{
 		const url = new URL(req.url);
-		log.log(env.logTopic||'ccw-log',req.url,'req keys',Object.keys(req))
+		log.log(env.logTopic||'ccw-log',req.url,{'req keys':Object.keys(req)})
 		if(req.method =='OPTIONS') {
 			return new Response('', { status: 200 ,headers:{
 				'Access-Control-Allow-Headers': '*',
@@ -91,6 +91,8 @@ export default {
 					else{
 						return new Response('没有配置测试token', { status: 500 });
 					}
+			default:
+			  log.log(env.logTopic||'ccw-log',req.url,'不支持的方法',url.pathname.split('/')[2])
 			}
 		}
 		return new Response('不正确的请求url',{status: 404});
