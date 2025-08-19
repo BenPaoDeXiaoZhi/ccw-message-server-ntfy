af2d4193dd211837a89ef0504c3fc5bfd8b54fa9
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-19 13:12:41 +0800

    更新 index.js

--- src/index.js
+++ src/index.js
@@ -91,8 +91,8 @@ export default {
 					else{
 						return new Response('没有配置测试token', { status: 500 });
 					}
-			default:
-			  log.log(env.logTopic||'ccw-log',req.url,'不支持的方法',url.pathname.split('/')[2])
+					default:
+				  log.log(env.logTopic||'ccw-log',req.url,'不支持的方法',url.pathname.split('/')[2])
 			}
 		}
 		return new Response('不正确的请求url',{status: 404});
