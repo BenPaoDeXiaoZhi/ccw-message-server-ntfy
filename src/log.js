b71739e71d00e2d1f83ceb0003b66f34a1e8d10c
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-19 13:02:20 +0800

    更新 log.js

--- src/log.js
+++ src/log.js
@@ -7,7 +7,7 @@ export function log(topicName,url,...args){
     }
     const ntfyMsg=new ntfyMessage.ntfyMessage(Date.now(),0,0,topicName,dat,"log from cf",["memo"])
     ntfyMsg.setTime(Math.floor(Date.now()/1000))
-    fetch('https://ntfy.sh/'+topicName, {
+    fetch('https://ntfy.sh/', {
         method: 'POST', // PUT works too
         body: JSON.stringify(ntfyMsg)
     })
