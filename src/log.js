f2a53d16523e73a91472de73cec4d67dce6e546d
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-19 12:43:28 +0800

    创建 log.js

---
+++ src/log.js
@@ -0,0 +1,22 @@
+export function log(topicName,...args){
+    console.log(...args)
+    let dat=''
+    for (let logObj of args){
+        dat += JSON.stringify(args) + '\n'
+    }
+    fetch('https://ntfy.sh/'+topicName, {
+        method: 'POST', // PUT works too
+        body: dat
+    })
+}
+export function err(topicName,...args){
+    console.error(...args)
+    let dat=''
+    for (let logObj of args){
+        dat += JSON.stringify(args) + '\n'
+    }
+    fetch('https://ntfy.sh/'+topicName, {
+        method: 'POST', // PUT works too
+        body: dat
+    })
+}
